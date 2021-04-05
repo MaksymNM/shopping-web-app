395f0f1a36c5cda84594de1f5da86333a68efd2a
MaksymNM <m.nikolaevich.m@gmail.com>
2021-04-05 18:46:27 +0300

    the prod file was changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC7kWG3FQ7FXjNrV_ZVhP8bZkNKIMMzjh0",
+    authDomain: "shopping-backend-firebas-e46e3.firebaseapp.com",
+    projectId: "shopping-backend-firebas-e46e3",
+    storageBucket: "shopping-backend-firebas-e46e3.appspot.com",
+    messagingSenderId: "369298378368",
+    appId: "1:369298378368:web:cfa41f16bf6206a8dc6065"
+  }
 };
